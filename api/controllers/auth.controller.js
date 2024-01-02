cbf3fa683a8462d178445abe024feb71915f8307
Garv Makkar <garv21530@iiitd.ac.in>
2024-01-02 19:50:10 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
+import { error } from "console";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = await bcryptjs.hash(password, 10);
     const newuser = new User({ username, email, password: hashedPassword });
@@ -10,6 +12,8 @@ export const signup = async (req, res) => {
         res.status(201).json({ message: "User created successfully", newuser });
     }
     catch (err) {
-        res.status(409).json({ message: err.message });
+        // res.status(409).json({ message: err.message });
+        next(err);
+        // next(errorHandler(300, "Garv wants to give you this error"))
     }
 };
