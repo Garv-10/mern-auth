2a8871e41e16445df3d31cbff83c348dc65e322e
Garv Makkar <garv21530@iiitd.ac.in>
2024-01-02 23:18:46 +0530

    complete signup page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <div className='bg-slate-200'>
-            <div className='flex  items-center justify-between max-w-4xl mx-auto p-4 md:p-8'>
+            <div className='flex  items-center justify-between max-w-4xl mx-auto md:p-4'>
                 <Link to='/'>
                     <h1 className='font-bold'>
                         Auth App
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,24 +1,81 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch('/api/auth/sign-up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await response.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setError(true);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder="Username" id="username" className="block mx-auto my-3 p-2 border-2 border-gray-300 rounded-md" />
-        <input type="email" placeholder="Email" id="email" className="block mx-auto my-3 p-2 border-2 border-gray-300 rounded-md" />
-        <input type="password" placeholder="Password" id="password" className="block mx-auto my-3 p-2 border-2 border-gray-300 rounded-md" />
-        <button type="submit" className="mx-auto my-3 p-2 px-5 border-2 rounded-md bg-slate-700 text-white font-semibold hover:opacity-90 disabled:opacity-80">Sign Up</button>
+      <h1 className='text-3xl text-center font-semibold my-7'>
+        Sign Up
+      </h1>
+      <form onSubmit={handleSubmit}
+        className='flex flex-col'>
+        <input type="text"
+          placeholder="Username"
+          id="username"
+          className="block mx-auto my-3 p-2 border-2 border-gray-300 rounded-md"
+          onChange={handleChange} />
+        <input type="email"
+          placeholder="Email"
+          id="email"
+          className="block mx-auto my-3 p-2 border-2 border-gray-300 rounded-md"
+          onChange={handleChange} />
+        <input type="password"
+          placeholder="Password"
+          id="password"
+          className="block mx-auto my-3 p-2 border-2 border-gray-300 rounded-md"
+          onChange={handleChange} />
+        <button disabled={loading}
+          type="submit"
+          className="mx-auto my-3 p-2 px-5 border-2 rounded-md bg-slate-700 text-white font-semibold hover:opacity-90 disabled:opacity-80">
+          {loading ? 'Loading' : 'Sign Up'}
+        </button>
       </form>
       <div className='flex justify-center items-center flex-col gap-2 mt-5 text-gray-500 text-sm'>
         <p>Already have an account?</p>
         <Link to='/sign-in' className='inline-block'>
-          <span className='text-blue-500 hover:underline'>Sign in</span>
+          <span className='text-blue-500 hover:underline'>
+            Sign in
+          </span>
         </Link>
       </div>
-
-
+      {error && <p className='text-red-700 text-center mt-5'>
+        Something went wrong!
+      </p>}
     </div>
-
   )
 }
