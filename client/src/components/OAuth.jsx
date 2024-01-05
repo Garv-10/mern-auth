cfa66f2c25adb57647fb5dd0616162480c2c77fe
Garv Makkar <garv21530@iiitd.ac.in>
2024-01-06 01:28:45 +0530

    completing update user functionality but i hav an error, lets handle it later

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,14 +3,15 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider();
             const auth = getAuth(app);
-
             const result = await signInWithPopup(auth, provider);
             const res = await fetch('/api/auth/google', {
                 method: 'POST',
@@ -25,6 +26,8 @@ export default function OAuth() {
             });
             const data = await res.json();
             dispatch(signInSuccess(data));
+            navigate('/');
+
         } catch (err) {
             console.log("Could not login woth google", err);
         }
