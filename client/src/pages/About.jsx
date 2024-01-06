452f86b0590891094a8265ec0030744ba58ce4b9
Garv Makkar <garv21530@iiitd.ac.in>
2024-01-06 16:05:01 +0530

    fix spelling in about us

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className='px-4 py-12 max-w-2xl mx-auto'>
             <h1 className='text-3xl font-bold mb-4 text-slate-800'>
-                Know About US
+                Know About Us
             </h1>
             <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum reprehenderit nisi iste voluptas sit, odio inventore quas et, voluptate maxime, adipisci earum. Hic, magni. Sapiente repellendus nemo, cum accusantium odit minus optio deserunt incidunt debitis quos ipsa ipsam earum beatae, culpa asperiores illum. Minus necessitatibus labore, dignissimos debitis ipsa architecto?</p>
             <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum reprehenderit nisi iste voluptas sit, odio inventore quas et, voluptate maxime, adipisci earum. Hic, magni. Sapiente repellendus nemo, cum accusantium odit minus optio deserunt incidunt debitis quos ipsa ipsam earum beatae, culpa asperiores illum. Minus necessitatibus labore, dignissimos debitis ipsa architecto?</p>
