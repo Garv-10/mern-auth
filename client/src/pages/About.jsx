f7221565e37ed69c2343c5c7250098c0d5a55ce9
Garv Makkar <garv21530@iiitd.ac.in>
2024-01-06 11:51:05 +0530

    home and about page done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className='px-4 py-12 max-w-2xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+                Know About US
+            </h1>
+            <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum reprehenderit nisi iste voluptas sit, odio inventore quas et, voluptate maxime, adipisci earum. Hic, magni. Sapiente repellendus nemo, cum accusantium odit minus optio deserunt incidunt debitis quos ipsa ipsam earum beatae, culpa asperiores illum. Minus necessitatibus labore, dignissimos debitis ipsa architecto?</p>
+            <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum reprehenderit nisi iste voluptas sit, odio inventore quas et, voluptate maxime, adipisci earum. Hic, magni. Sapiente repellendus nemo, cum accusantium odit minus optio deserunt incidunt debitis quos ipsa ipsam earum beatae, culpa asperiores illum. Minus necessitatibus labore, dignissimos debitis ipsa architecto?</p>
+            <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum reprehenderit nisi iste voluptas sit, odio inventore quas et, voluptate maxime, adipisci earum. Hic, magni. Sapiente repellendus nemo, cum accusantium odit minus optio deserunt incidunt debitis quos ipsa ipsam earum beatae, culpa asperiores illum. Minus necessitatibus labore, dignissimos debitis ipsa architecto?</p>
         </div>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Welcome to my Authentication App!
+      </h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur in facilis repudiandae alias exercitationem numquam. Corrupti, deleniti voluptates. Non, error optio quia dolores aspernatur iste dolorem neque nostrum enim labore voluptate earum distinctio minus, obcaecati consequuntur deleniti. Obcaecati natus optio assumenda esse vitae repellendus perspiciatis vel! Maxime expedita suscipit deserunt!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur in facilis repudiandae alias exercitationem numquam. Corrupti, deleniti voluptates. Non, error optio quia dolores aspernatur iste dolorem neque nostrum enim labore voluptate earum distinctio minus, obcaecati consequuntur deleniti. Obcaecati natus optio assumenda esse vitae repellendus perspiciatis vel! Maxime expedita suscipit deserunt!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur in facilis repudiandae alias exercitationem numquam. Corrupti, deleniti voluptates. Non, error optio quia dolores aspernatur iste dolorem neque nostrum enim labore voluptate earum distinctio minus, obcaecati consequuntur deleniti. Obcaecati natus optio assumenda esse vitae repellendus perspiciatis vel! Maxime expedita suscipit deserunt!</p>
     </div>
   )
 }
